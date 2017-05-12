c65e756c08b0b97ac8782927bbde035eb989a4f1
Rudraksh <Rudraksh>
2017-05-12 16:19:10 +0530

    package used to login

--- src/app/app-globals.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Global constants. You can define your constant and access it by importing this file in the component.
- */
-export const AppGlobals = {
-    GOOGLE_CLIENT_ID: '903075759019-3q89hsrh5q0p8kjvk4etv9l8d6qflqro.apps.googleusercontent.com'
-}
--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -1,35 +1,37 @@
 import { Component, OnInit, NgZone } from '@angular/core';
-import { AuthService } from './google-authentication/auth.service';
+import { AuthService, AppGlobals } from 'angular2-google-login';
 
 @Component({
   selector: 'app-root',
   templateUrl: './login.component.html',
-  providers:[AuthService]
+  providers: [AuthService]
 })
-export class LoginComponent implements OnInit{
-  imageURL  : string;
-  email : string;
-  name  : string; 
-  token  : string; 
+export class LoginComponent implements OnInit {
+  imageURL: string;
+  email: string;
+  name: string;
+  token: string;
 
-  constructor(private auth :AuthService, private zone:NgZone){}
+  constructor(private auth: AuthService, private zone: NgZone) { }
 
   /**
    * Ininitalizing Google Authentication API and getting data from localstorage if logged in
    */
-  ngOnInit(){
-     this.getData();
-     setTimeout(()=>{this.googleAuthenticate()},50);
+  ngOnInit() {
+    //Set your Google Client ID here
+    AppGlobals.GOOGLE_CLIENT_ID = 'SECRET_CLIENT_ID';
+    this.getData();
+    setTimeout(() => { this.googleAuthenticate() }, 50);
   }
 
   /**
    * Calling Google Authentication service
    */
-  googleAuthenticate(){
-    this.auth.authenticateUser((result)=>{
+  googleAuthenticate() {
+    this.auth.authenticateUser((result) => {
       //Using Angular2 Zone dependency to manage the scope of variables
       this.zone.run(() => {
-          this.getData();
+        this.getData();
       });
     });
   }
@@ -37,7 +39,7 @@ export class LoginComponent implements OnInit{
   /**
    * Getting data from browser's local storage
    */
-  getData(){
+  getData() {
     this.token = localStorage.getItem('token');
     this.imageURL = localStorage.getItem('image');
     this.name = localStorage.getItem('name');
@@ -47,17 +49,17 @@ export class LoginComponent implements OnInit{
   /**
    * Logout user and calls function to clear the localstorage
    */
-  logout(){
+  logout() {
     let scopeReference = this;
-    this.auth.userLogout(function(){
+    this.auth.userLogout(function () {
       scopeReference.clearLocalStorage();
     });
   }
-  
+
   /**
    * Clearing Localstorage of browser
    */
-  clearLocalStorage(){
+  clearLocalStorage() {
     localStorage.removeItem('token');
     localStorage.removeItem('image');
     localStorage.removeItem('name');
